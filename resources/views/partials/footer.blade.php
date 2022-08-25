674c63195526cf62f89e0210a3fe127e8986a4c5
franciowel <spina_francesco@blu.it>
2022-08-25 15:38:02 +0200

    upload footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,65 @@
+<footer>
+    <div>
+        <div class="back">
+            <div class="container">
+                <div class="flex">
+                    <div>
+                        <div class="footer-links">
+                            <span class="title">DC COMICS</span>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                        </div>
+                        <div class="footer-links">
+                            <span class="title">SHOP</span>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="footer-links">
+                            <span class="title">DC</span>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="footer-links">
+                            <span class="title">SITES</span>
+                            <ul>
+                                <li>Videos</li>
+                                <li>News</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img class="logo" src="{{ asset('img/dc-logo-bg.png') }}" alt="logo">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
